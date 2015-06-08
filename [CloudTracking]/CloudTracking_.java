972ccb00b55966c92bdfbc68cc5589721c4887f0
JualianeT <juli.turzynski@freenet.de>
2015-06-08 23:49:27 +0200

    Binarisierung an ImageJ angepasst. Binarisierung in CloudTracking eingefügt. Binarisierte Bilder anzeigen

--- [CloudTracking]/CloudTracking_.java
+++ [CloudTracking]/CloudTracking_.java
@@ -6,24 +6,28 @@ import ij.plugin.PlugIn;
 import ij.plugin.filter.Analyzer;
 import ij.process.ImageProcessor;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import cloud.Binarisierung;
 import cloud.Cloud;
 import cloud.CloudPair;
+import cloud.RGBToHSB;
 import cloud.Vec2;
 
 public class CloudTracking_ implements PlugIn {
 
 	private FolderOpener opener;
 	private ImagePlus clouds;
-	
+
 	private ImageProcessor ref;
 	private ImageProcessor corr;
-	private String dirWorkFolder = ".../cloudFolder";
-	private String dirImgRepo = ".../cloudImgRepo";
+	private String dirWorkFolder = "./cloudFolder";
+	private String dirImgRepo = "./cloudImgRepo";
 	private String filename = "cloud0";
 	private String ending = ".png";
 	private String commandDelete;
@@ -33,248 +37,279 @@ public class CloudTracking_ implements PlugIn {
 	private ArrayList<Cloud> correspondenceList;
 	private ArrayList<CloudPair> pairList;
 	private ResultsTable results;
-	
+
 	private Process p;
 	private int numOfIteration = 1;
-	
-	
+
 	public void run(String arg) {
 
-	while(numOfIteration > 0){	
-		
-		ImageStack cloudStack = loadImagesIntoStack();
-		
-		// an dieser Stelle erfolgt die Binarisierung des Stacks
-		makeBinaryStack();
-		
-		// aus dem Stack wird das Referenzbild sowie das Korrespondenzbild extrahiert
-		ref = cloudStack.getProcessor(1);
-		corr = cloudStack.getProcessor(2);
-		
-		
-		// Für die Bewegungsprognose wird aus dem vorherigen Durchlauf der jeweilige Vektor und die Richtung aus der ResultsTable ausgelesen
-		
-		// Wenn keine Bewegungsprognose aus der vorherigen Iteration vorhanden ist bzw. gleich Null ist,
-		// wird die findBorders-Methode aufgerufen, welche jeweils eine List mit Cloud-Objekten zurück gibt.
-		referenceList = findBorders(ref);
-		correspondenceList = findBorders(corr);
+		while (numOfIteration > 0) {
 
-		
-		// Wenn eine Prognose aus dem vorherigen Durchlauf vorhanden ist, dann wir das Tracking durchgeführt.
-		// Tracking
-		
-		pairList = findCloudPairs(referenceList, correspondenceList);
-		
-		// Der ResultsTable wird am Ende des Durchlaufs die Wolke inkl. Prognose hinzugefügt und innerhalb von ImageJ als Auflistung angezeigt
-		// welche Methode hierzu benutzt wird, ist noch unklar, vielleicht muss diese noch implementiert werden
-		
-		
-		ResultsTable rt = Analyzer.getResultsTable();
-		if (rt == null) {
-		        rt = new ResultsTable();
-		        Analyzer.setResultsTable(rt);
-		}
-		
-		for (int i = 0; i < pairList.size(); i++){
+			ImageStack cloudStack = loadImagesIntoStack();	//TODO: ist das nicht unperformant? wie viele bilder werden denn mit jeder iteration geladen?			
 			
-		rt.incrementCounter();
-		
-		rt.addValue("NumberOfIteration", numOfIteration);
-		rt.addValue("CloudNumber", -1);
-		rt.addValue("Time until Sun is reached (sec)", pairList.get(i).timeUntilSunIsReached(30, 30));
-		rt.addValue("Cloud velocity (pix per sec)", pairList.get(i).calculateVelocity());
-		rt.addValue("Reference Cloud Height", pairList.get(i).getReference().getHeight());
-		rt.addValue("Reference Cloud Width", pairList.get(i).getReference().getWidth());
-		rt.addValue("Reference Cloud Center_X", pairList.get(i).getReference().getX());
-		rt.addValue("Reference Cloud Center_Y", pairList.get(i).getReference().getY());
-		rt.addValue("Correspondence Cloud Height", pairList.get(i).getCorrespondence().getHeight());
-		rt.addValue("Correspondence Cloud Width", pairList.get(i).getCorrespondence().getWidth());
-		rt.addValue("Correspondence Cloud Center_X", pairList.get(i).getCorrespondence().getX());
-		rt.addValue("Correspondence Cloud Center_Y", pairList.get(i).getCorrespondence().getY());
-		
+			// an dieser Stelle erfolgt die Binarisierung des Stacks
+			makeBinaryStack(cloudStack);
+
+			// aus dem Stack wird das Referenzbild sowie das Korrespondenzbild
+			// extrahiert
+			ref = cloudStack.getProcessor(1);
+			corr = cloudStack.getProcessor(2);
+			
+			//Ausgabe des binarisierten Bildes
+			new ImagePlus("ref", ref).show();
+			new ImagePlus("corr", corr).show();
+			
+			
+
+			// F�r die Bewegungsprognose wird aus dem vorherigen Durchlauf der
+			// jeweilige Vektor und die Richtung aus der ResultsTable ausgelesen
+
+			// Wenn keine Bewegungsprognose aus der vorherigen Iteration
+			// vorhanden ist bzw. gleich Null ist,
+			// wird die findBorders-Methode aufgerufen, welche jeweils eine List
+			// mit Cloud-Objekten zur�ck gibt.
+			referenceList = findBorders(ref);
+			correspondenceList = findBorders(corr);
+
+			// Wenn eine Prognose aus dem vorherigen Durchlauf vorhanden ist,
+			// dann wir das Tracking durchgeführt.
+			// Tracking
+
+			pairList = findCloudPairs(referenceList, correspondenceList);
+
+			// Der ResultsTable wird am Ende des Durchlaufs die Wolke inkl.
+			// Prognose hinzugefügt und innerhalb von ImageJ als Auflistung
+			// angezeigt
+			// welche Methode hierzu benutzt wird, ist noch unklar, vielleicht
+			// muss diese noch implementiert werden
+
+			ResultsTable rt = Analyzer.getResultsTable();
+			if (rt == null) {
+				rt = new ResultsTable();
+				Analyzer.setResultsTable(rt);
+			}
+
+			for (int i = 0; i < pairList.size(); i++) {
+
+				rt.incrementCounter();
+
+				rt.addValue("NumberOfIteration", numOfIteration);
+				rt.addValue("CloudNumber", -1);
+				rt.addValue("Time until Sun is reached (sec)", pairList.get(i)
+						.timeUntilSunIsReached(30, 30));
+				rt.addValue("Cloud velocity (pix per sec)", pairList.get(i)
+						.calculateVelocity());
+				rt.addValue("Reference Cloud Height", pairList.get(i)
+						.getReference().getHeight());
+				rt.addValue("Reference Cloud Width", pairList.get(i)
+						.getReference().getWidth());
+				rt.addValue("Reference Cloud Center_X", pairList.get(i)
+						.getReference().getX());
+				rt.addValue("Reference Cloud Center_Y", pairList.get(i)
+						.getReference().getY());
+				rt.addValue("Correspondence Cloud Height", pairList.get(i)
+						.getCorrespondence().getHeight());
+				rt.addValue("Correspondence Cloud Width", pairList.get(i)
+						.getCorrespondence().getWidth());
+				rt.addValue("Correspondence Cloud Center_X", pairList.get(i)
+						.getCorrespondence().getX());
+				rt.addValue("Correspondence Cloud Center_Y", pairList.get(i)
+						.getCorrespondence().getY());
+
+			}
+
+			rt.show("Results");
+
+			// Die folgende Methode bereitet das Verzeichnis für den nächsten
+			// Durchlauf vor
+			// prepareNextIteration();
+
+			numOfIteration++;
+
+			// Hier wird die Aufnahme der Kamera simuliert
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+
 		}
-		
-		rt.show("Results");
-		
-		// Die folgende Methode bereitet das Verzeichnis für den nächsten Durchlauf vor
-		//prepareNextIteration();
-		
-		numOfIteration++;
-		
-		
-		// Hier wird die Aufnahme der Kamera simuliert
-		try {
-			Thread.sleep(3000);
-		} catch(InterruptedException ex) {
-		    Thread.currentThread().interrupt();
-		}
-		
 	}
-	}
-	
-	private ImageStack loadImagesIntoStack(){
-		
+
+	private ImageStack loadImagesIntoStack() {
 
 		opener = new FolderOpener();
 		clouds = new ImagePlus();
 		clouds = opener.openFolder(dirWorkFolder);
-		
+
 		return clouds.getImageStack();
-			
 	}
-	
-	private void makeBinaryStack(){};
-	
-	private ArrayList<Cloud> findBorders(ImageProcessor binary){
+
+	private void makeBinaryStack(ImageStack cloudStack) {
+
+		Binarisierung.binaryPicture(cloudStack.getProcessor(1));
+		Binarisierung.binaryPicture(cloudStack.getProcessor(2));
+	}
+
+	private ArrayList<Cloud> findBorders(ImageProcessor binary) {
 
 		binary.autoThreshold();
-		
+
 		ArrayList<Cloud> cloudList = new ArrayList<Cloud>();
-		
+
 		int white = -1;
 		int width = binary.getWidth();
 		int height = binary.getHeight();
-	
-		byte [] pixels = (byte[]) binary.getPixels();
-		
+
+		byte[] pixels = (byte[]) binary.getPixels();
+
 		int length = pixels.length;
-		
-		boolean [] marker = new boolean[pixels.length];
-		
-		
-		for(int y = 0; y < height; y++ ){
-			for(int x = 0; x < width; x++){
-				if(pixels[y*width + x] == white && marker[y*width + x] == false){
-					
-					int[] xy={x,y};
-					
+
+		boolean[] marker = new boolean[pixels.length];
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (pixels[y * width + x] == white
+						&& marker[y * width + x] == false) {
+
+					int[] xy = { x, y };
+
 					int centerX = 0, centerY = 0, cloudWidth = 0, cloudHeight = 0;
 					int minX = x, minY = y, maxX = x, maxY = y;
-					
-					Queue <int[]> fifo = new LinkedList();
-					fifo.add(xy);
-					
-					while(fifo.isEmpty() == false){
-						
-						int [] coordXY = fifo.poll();
-						
-						if(coordXY[1] * width + coordXY[0] - 1 > 0){
-						
-							if(pixels[coordXY[1] * width + coordXY[0] - 1] == white && marker[coordXY[1] * width + coordXY[0] - 1]==false){
-							
-								int[] xyL={coordXY[0] - 1,coordXY[1]};
-								fifo.add(xyL);
-								if(xyL[0]<minX)
-									minX=xyL[0];
-								
-								marker[coordXY[1] * width + coordXY[0]] = true;
-								}
-							
-							
-						}
-						
-						if(coordXY[1] * width + coordXY[0] + 1 < length){
-						
-							if(pixels[coordXY[1] * width + coordXY[0] + 1]== white && marker[coordXY[1] * width + coordXY[0] + 1]==false){
-							
-								int[] xyR={coordXY[0] + 1, coordXY[1]};
-								fifo.add(xyR);
-								if(xyR[0]>maxX)
-									maxX=xyR[0];
-								
-								marker[coordXY[1] * width + coordXY[0]] = true;
-								}
-						}
-						
-						if((coordXY[1] - 1) * width + coordXY[0] > 0){
-						
-							if(pixels[(coordXY[1]-1) * width + coordXY[0]] == white && marker[(coordXY[1]-1) * width + coordXY[0]] == false){
-							
-								int[] xyO={coordXY[0],coordXY[1]-1};
-								fifo.add(xyO);
-								if(xyO[1] * width < minY)
-									minY=xyO[1];
-							
-								marker[coordXY[1] * width + coordXY[0]] = true;
-								}
-						}
-						
-						
-						if((coordXY[1] + 1) * width + coordXY[0] < length){
-							
-							if(pixels[(coordXY[1]+1) * width + coordXY[0]] == white && marker[(coordXY[1]+1) * width + coordXY[0]]==false){
-								
-								int[] xyU={coordXY[0],coordXY[1]+1};
-								fifo.add(xyU);
-								if(xyU[1]* width >maxY)
-									maxY=xyU[1];
-								
-								marker[coordXY[1] * width + coordXY[0]] = true;
-								}
-						}
-					
-					
-					}
-					
-					centerX=minX + (maxX-minX)/2;
-					centerY= minY + (maxY-minY)/2;
-					cloudWidth=maxX-minX;
-					cloudHeight=maxY-minY;
-					
-					Cloud tmp = new Cloud(centerX,centerY,cloudWidth,cloudHeight);
 
-					if(tmp.getHeight() != 0 && tmp.getWidth() != 0){
+					Queue<int[]> fifo = new LinkedList();
+					fifo.add(xy);
+
+					while (fifo.isEmpty() == false) {
+
+						int[] coordXY = fifo.poll();
+
+						if (coordXY[1] * width + coordXY[0] - 1 > 0) {
+
+							if (pixels[coordXY[1] * width + coordXY[0] - 1] == white
+									&& marker[coordXY[1] * width + coordXY[0]
+											- 1] == false) {
+
+								int[] xyL = { coordXY[0] - 1, coordXY[1] };
+								fifo.add(xyL);
+								if (xyL[0] < minX)
+									minX = xyL[0];
+
+								marker[coordXY[1] * width + coordXY[0]] = true;
+							}
+
+						}
+
+						if (coordXY[1] * width + coordXY[0] + 1 < length) {
+
+							if (pixels[coordXY[1] * width + coordXY[0] + 1] == white
+									&& marker[coordXY[1] * width + coordXY[0]
+											+ 1] == false) {
+
+								int[] xyR = { coordXY[0] + 1, coordXY[1] };
+								fifo.add(xyR);
+								if (xyR[0] > maxX)
+									maxX = xyR[0];
+
+								marker[coordXY[1] * width + coordXY[0]] = true;
+							}
+						}
+
+						if ((coordXY[1] - 1) * width + coordXY[0] > 0) {
+
+							if (pixels[(coordXY[1] - 1) * width + coordXY[0]] == white
+									&& marker[(coordXY[1] - 1) * width
+											+ coordXY[0]] == false) {
+
+								int[] xyO = { coordXY[0], coordXY[1] - 1 };
+								fifo.add(xyO);
+								if (xyO[1] * width < minY)
+									minY = xyO[1];
+
+								marker[coordXY[1] * width + coordXY[0]] = true;
+							}
+						}
+
+						if ((coordXY[1] + 1) * width + coordXY[0] < length) {
+
+							if (pixels[(coordXY[1] + 1) * width + coordXY[0]] == white
+									&& marker[(coordXY[1] + 1) * width
+											+ coordXY[0]] == false) {
+
+								int[] xyU = { coordXY[0], coordXY[1] + 1 };
+								fifo.add(xyU);
+								if (xyU[1] * width > maxY)
+									maxY = xyU[1];
+
+								marker[coordXY[1] * width + coordXY[0]] = true;
+							}
+						}
+
+					}
+
+					centerX = minX + (maxX - minX) / 2;
+					centerY = minY + (maxY - minY) / 2;
+					cloudWidth = maxX - minX;
+					cloudHeight = maxY - minY;
+
+					Cloud tmp = new Cloud(centerX, centerY, cloudWidth,
+							cloudHeight);
+
+					if (tmp.getHeight() != 0 && tmp.getWidth() != 0) {
 						cloudList.add(tmp);
 					}
 				}
 			}
 		}
-		
+
 		return cloudList;
 	}
-	
-	private ArrayList<CloudPair> findCloudPairs(ArrayList<Cloud> referenceList, ArrayList<Cloud> correspondenceList){
-	
+
+	private ArrayList<CloudPair> findCloudPairs(ArrayList<Cloud> referenceList,
+			ArrayList<Cloud> correspondenceList) {
+
 		ArrayList<CloudPair> pairs = new ArrayList<CloudPair>();
-		
-		for(int i = 0; i < referenceList.size(); i++){
-			
+
+		for (int i = 0; i < referenceList.size(); i++) {
+
 			Cloud ref = referenceList.get(i);
-			for(int j = 0; j < correspondenceList.size(); j++){
-				
+			for (int j = 0; j < correspondenceList.size(); j++) {
+
 				Cloud corr = correspondenceList.get(j);
-				if(ref.findMatchingCloud(corr)){
-					
+				if (ref.findMatchingCloud(corr)) {
+
 					Vec2 tmp = ref.computeCenterDistance(corr);
-					
-					CloudPair pairTmp = new CloudPair(ref, corr, tmp, numOfIteration);
-					pairs.add(pairTmp);		
+
+					CloudPair pairTmp = new CloudPair(ref, corr, tmp,
+							numOfIteration);
+					pairs.add(pairTmp);
 				}
-			}	
+			}
 		}
-		return pairs;	
+		return pairs;
 	}
-	
-	private void prepareNextIteration(){
+
+	private void prepareNextIteration() {
 		// unix-shell command
-		commandDelete = "rm " + dirWorkFolder + "/" + filename + numOfIteration +  ending;	
-		
-		// Vorerst werden Bilder aus einem externen Ordner geladen, 
+		commandDelete = "rm " + dirWorkFolder + "/" + filename + numOfIteration
+				+ ending;
+
+		// Vorerst werden Bilder aus einem externen Ordner geladen,
 		// in einer späteren Umsetzung liefert hier die Kamera Bilder
-		
-		commandLoad = "mv " + dirImgRepo + "/" + filename + (numOfIteration + 2) + ending;
-		
+
+		commandLoad = "mv " + dirImgRepo + "/" + filename
+				+ (numOfIteration + 2) + ending;
+
 		try {
 			p = Runtime.getRuntime().exec(commandDelete);
 			p = Runtime.getRuntime().exec(commandLoad);
 			System.out.println(commandDelete);
 			System.out.println(commandLoad);
-			
+
 		} catch (IOException e) {
-		// TODO Auto-generated catch block
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 	}
 
--- [CloudTracking]/cloud/Binarisierung.java
+++ [CloudTracking]/cloud/Binarisierung.java
@@ -1,29 +1,58 @@
 package cloud;
 
+import ij.process.ImageProcessor;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 public class Binarisierung {
-	
+
 	/**
-	 * Erzeugen eines Binärbildes
-	 * mit true = weiß --> Wolke
-	 * und false = schwarz --> Himmel
+	 * Erzeugen eines Binärbildes mit true = weiß --> Wolke und false = schwarz
+	 * --> Himmel
+	 * 
 	 * @param HSBArray
 	 * @return
 	 */
-	public static boolean[][] binaryPicture (float[][][] HSBArray){
+	public static void binaryPicture(ImageProcessor proc) {
+
+		Image image = proc.createImage();
+
+		// Cast von Image in BufferedImage
+		BufferedImage img = (BufferedImage) image;
 		
-		boolean[][] binaryPicture =  new boolean[HSBArray.length][HSBArray[0].length];
-		
-		for(int i = 0; i < HSBArray.length; i++){
-			for(int j = 0; j < HSBArray[i].length; j++){
-				
-				//Testen auf nicht Himmel (Wolke)
-				 if(HSBArray[i][j][1] < 0.3){
-					 binaryPicture[i][j] = true;
-				 }
+		// Erzeugen des ColorArray
+		Color[][] colorImage = new Color[img.getWidth()][img.getHeight()];
+
+		// Umwandlung des Image in ein ColorArray
+		for (int i = 0; i < colorImage.length; i++) {
+			for (int j = 0; j < colorImage[i].length; j++) {
+				int colorAsInt = img.getRGB(i, j);
+				int red = (colorAsInt & 0x00ff0000) >> 16;
+				int green = (colorAsInt & 0x0000ff00) >> 8;
+				int blue = colorAsInt & 0x000000ff;
+
+				colorImage[j][i] = new Color(red, green, blue);
 			}
 		}
 		
-		return binaryPicture;
+		float[][][] HSBArray = RGBToHSB.pixelArray(colorImage);
+
+		
+		//Binarisierung
+		for (int i = 0; i < HSBArray.length; i++) {
+			for (int j = 0; j < HSBArray[i].length; j++) {
+
+				// Testen auf nicht Himmel (Wolke)
+				if (HSBArray[i][j][1] < 0.3) {
+					
+					proc.putPixel(i, j, 255);
+				} else {
+
+					proc.putPixel(i, j, 0);
+				}
+			}
+		}
 	}
-	
 }
